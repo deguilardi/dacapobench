335a6972e1ce5b2dc9471e0ba73537d91d3ca6bb
Demetrio Guilardi <demetrio.guilardi@gmail.com>
2020-01-02 05:55:19 +0000

    tab to spaces

--- benchmarks/agent_uqac/src/ca/uqac/Transformer.java
+++ benchmarks/agent_uqac/src/ca/uqac/Transformer.java
@@ -19,63 +19,63 @@ import javassist.NotFoundException;
 
 public class Transformer implements ClassFileTransformer{
 
-	private static String TAG = "Transformer";
+    private static String TAG = "Transformer";
 
-	private static String[] includedPackages = { 
-		// "Harness",
-		// "org/dacapo",
-		"java/io",
-		"avrora/core",
-		"avrora/Main",
+    private static String[] includedPackages = { 
+        // "Harness",
+        // "org/dacapo",
+        "java/io",
+        "avrora/core",
+        "avrora/Main",
 
-		"org/apache",
-	};
-	private static HashSet<String> includedPackagesHash = new HashSet<String>( Arrays.asList( includedPackages ) );
+        "org/apache",
+    };
+    private static HashSet<String> includedPackagesHash = new HashSet<String>( Arrays.asList( includedPackages ) );
 
-	@Override
+    @Override
     public byte[] transform( ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer ){
-		String classPackage = extractPackage( className );
-		if( !includedPackagesHash.contains( classPackage ) ){
-			return classfileBuffer;
-		}
-
-		Logger.log( TAG, Logger.DEBUG, "loading class: " + className );
-		CtClass cl = null;
-		try{
-			ClassPool pool = ClassPool.getDefault();
-			cl = pool.makeClass( new java.io.ByteArrayInputStream( classfileBuffer ) );
-		    CtBehavior[] methods = cl.getDeclaredBehaviors();
-		    for( CtBehavior method : methods ){
-		        transformMethod( method, className );
-		    }
-		    classfileBuffer = cl.toBytecode();
-		}
-		catch( NotFoundException | CannotCompileException | IOException e ){
-			Logger.log( TAG, Logger.ERROR, e.getMessage() );
-		}
-		finally{
-			cl.detach();
-		}
-		return classfileBuffer;
-	}
-
-	private void transformMethod( CtBehavior method, String className ) throws NotFoundException, CannotCompileException{
-		Logger.log( TAG, Logger.DEBUG, "loading method: " + method.getName() );
-		int modifiers = method.getModifiers();
-		if(    !method.isEmpty()
-		    && !Modifier.isNative( modifiers ) 
-			&&  Modifier.isPublic( modifiers )
-		){
-			String message = Logger.format( "call", Logger.INFO, className + "::" + method.getName() );
-            String insertString = "System.out.println( \"[\" + System.currentTimeMillis() + \"]" + message + "\");";
-	        method.insertBefore( insertString );
+        String classPackage = extractPackage( className );
+        if( !includedPackagesHash.contains( classPackage ) ){
+            return classfileBuffer;
         }
-	}
 
-	private String extractPackage( String input ){
-		Pattern pattern = Pattern.compile( "([A-Za-z0-9]{1,}.[A-Za-z0-9]{1,})" );
-		Matcher matcher = pattern.matcher( input );
-		matcher.find();
-		return matcher.group( 1 );
-	}
+        Logger.log( TAG, Logger.DEBUG, "loading class: " + className );
+        CtClass cl = null;
+        try{
+            ClassPool pool = ClassPool.getDefault();
+            cl = pool.makeClass( new java.io.ByteArrayInputStream( classfileBuffer ) );
+            CtBehavior[] methods = cl.getDeclaredBehaviors();
+            for( CtBehavior method : methods ){
+                transformMethod( method, className );
+            }
+            classfileBuffer = cl.toBytecode();
+        }
+        catch( NotFoundException | CannotCompileException | IOException e ){
+        	Logger.log( TAG, Logger.ERROR, e.getMessage() );
+        }
+        finally{
+            cl.detach();
+        }
+        return classfileBuffer;
+    }
+
+    private void transformMethod( CtBehavior method, String className ) throws NotFoundException, CannotCompileException{
+        Logger.log( TAG, Logger.DEBUG, "loading method: " + method.getName() );
+        int modifiers = method.getModifiers();
+        if(    !method.isEmpty()
+    	    && !Modifier.isNative( modifiers ) 
+    	    &&  Modifier.isPublic( modifiers )
+        ){
+            String message = Logger.format( "call", Logger.INFO, className + "::" + method.getName() );
+            String insertString = "System.out.println( \"[\" + System.currentTimeMillis() + \"]" + message + "\");";
+            method.insertBefore( insertString );
+        }
+    }
+
+    private String extractPackage( String input ){
+        Pattern pattern = Pattern.compile( "([A-Za-z0-9]{1,}.[A-Za-z0-9]{1,})" );
+        Matcher matcher = pattern.matcher( input );
+        matcher.find();
+        return matcher.group( 1 );
+    }
 }
