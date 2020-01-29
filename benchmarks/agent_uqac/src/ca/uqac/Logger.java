c1ab6c060bbf20c154835affb26868c9adc0c974
deguilardi <demetrio.guilardi@gmail.com>
2020-01-29 10:32:34 -0500

    agent generates class list

---
+++ benchmarks/agent_uqac/src/ca/uqac/Logger.java
@@ -0,0 +1,34 @@
+package ca.uqac;
+
+public abstract class Logger{
+
+	public static String INFO = "info";
+	public static String DEBUG = "debug";
+	public static String ERROR = "error";
+
+	private static String currentLevel = DEBUG;
+
+	public static void log( String tag, String level, String message ){
+		if( checkLevel( level ) ){
+			String output = format( tag, level, message);
+			System.out.println( "[" + System.currentTimeMillis() + "]" +  output );
+		}
+	}
+
+	public static String format( String tag, String level, String message ){
+		return "[" + tag + "][" + level + "] " + message;
+	}
+
+	private static boolean checkLevel( String level ){
+		if( currentLevel.equals( INFO ) ){
+			return true;
+		}
+		else if( currentLevel.equals( DEBUG ) && !level.equals( INFO ) ){
+			return true;
+		}
+		else if( currentLevel.equals( level ) ){
+			return true;
+		}
+		return false;
+	}
+}
--- benchmarks/agent_uqac/src/ca/uqac/UqacAgent.java
+++ benchmarks/agent_uqac/src/ca/uqac/UqacAgent.java
@@ -1,7 +1,8 @@
 package ca.uqac;
 
-import java.lang.ClassLoader;
 import java.lang.instrument.Instrumentation;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.jar.JarFile;
@@ -10,29 +11,54 @@ import java.util.List;
 class UqacAgent{
 
 	private static String TAG = "UqacAgent";
+	private static String ARG_EXAMINE = "examine";
+	private static String ARG_TRACE = "trace";
+	private static String FILE_CLASSES = "./classesList.txt";
+
 	private static List<Class> loadedClasses = new ArrayList<Class>();
 
 	public static void premain( String agentArgs, Instrumentation inst ){
-	    Logger.log( TAG, Logger.INFO, "premain method called" );
+		Logger.log( TAG, Logger.INFO, "premain method called" );
+		
 
-	    // extractClasses( inst );
-	    registerTransformer( inst );
+		if( agentArgs == null || agentArgs.equals( "" ) ){
+			Logger.log( TAG, Logger.INFO, "no argument, doing nothing" );
+		}
+		else if( agentArgs.equals( ARG_EXAMINE ) ){
+			Logger.log( TAG, Logger.INFO, "examining, a classes list will be extracted" );
+			try{
+				examineClasses( inst );
+			}
+			catch(IOException e){
+				Logger.log( TAG, Logger.ERROR, e.getMessage() );
+			}
+		}
+		else if( agentArgs.equals( ARG_TRACE ) ){
+			Logger.log( TAG, Logger.INFO, "tracing, the defined classes will be traced" );
+			registerTransformer( inst );
+		}
+		else{
+			Logger.log( TAG, Logger.ERROR, "wrong argument, 'examine' and 'trace' are valid arguments" );
+		}
+
 	    // transformLoadedClasses();
-
 	    // String className = "Harness";
 	    // transformClass( className, inst );
 	}
 
-	// private static void extractClasses( Instrumentation inst ){
-	// 	Class[] classes = inst.getAllLoadedClasses();
-	//     for( Class c : classes ){
-	//         if( inst.isModifiableClass(c) && inst.isRetransformClassesSupported() ){
-	//             loadedClasses.add(c);
-	//         }
-	//     }
-
-	//     Logger.log( TAG, Logger.INFO, "Found " + loadedClasses.size() + " classes to be transformed" );
-	// }
+	private static void examineClasses( Instrumentation inst ) throws IOException {
+		BufferedWriter writer = new BufferedWriter( new FileWriter( FILE_CLASSES ) );
+		Class[] classes = inst.getAllLoadedClasses();
+	    for( Class c : classes ){
+	        if( inst.isModifiableClass(c) && inst.isRetransformClassesSupported() && !c.getName().startsWith( "ca.uqac" ) ){
+	            loadedClasses.add(c);
+				Logger.log( TAG, Logger.DEBUG, c.getName() );
+				writer.write( c.getName() + "\n" );
+	        }
+		}
+		writer.close();
+	    Logger.log( TAG, Logger.INFO, "Found " + loadedClasses.size() + " classes that could be transformed" );
+	}
 
 	private static void registerTransformer( Instrumentation inst ){
 		try{
